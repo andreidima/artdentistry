2db19c806bf89e8132cdd333b05caf01ecb4562b
‘Andrei <andrei.dima@usm.ro>
2022-07-29 11:46:25 +0300

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <script src="{{ asset('js/app.js?v='.filemtime(public_path('js/app.js'))) }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js?v='.filemtime(public_path('js/app.js'))) }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
