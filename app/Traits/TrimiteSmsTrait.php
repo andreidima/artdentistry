68dfb8a8a094b663bb82bb9e1badd417b3880d0a
Andrei <andrei.dima@usm.ro>
2025-01-28 10:01:04 +0100

    a

--- app/Traits/TrimiteSmsTrait.php
+++ app/Traits/TrimiteSmsTrait.php
@@ -19,7 +19,7 @@ trait TrimiteSmsTrait {
         // New version to remove diacritics
         $transliterated = iconv('UTF-8', 'ASCII//TRANSLIT', $mesaj);
         // Allow letters, numbers, spaces, and common punctuation marks
-        $mesaj = preg_replace('/[^A-Za-z0-9 .,!?]/', '', $transliterated); // Remove non-ASCII characters
+        $mesaj = preg_replace('/[^A-Za-z0-9 .,!?;:]/', '', $transliterated); // Remove non-ASCII characters
 
         // Setarea trimiterii live sau testarea sms-ului
         // $test = 1; // sms-ul nu se trimite
